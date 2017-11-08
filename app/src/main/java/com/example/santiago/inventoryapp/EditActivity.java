2ebbf16c6b6a2b6a714174a80674df8d695cc154
josancamon <joan.santiago.cabezas@com>
2017-11-07 21:16:21 -0500

    without image 1/2

--- app/src/main/java/com/example/santiago/inventoryapp/EditActivity.java
+++ app/src/main/java/com/example/santiago/inventoryapp/EditActivity.java
@@ -1,10 +1,16 @@
 package com.example.santiago.inventoryapp;
 
+import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
 import android.net.Uri;
 import android.os.Build;
 import android.support.v4.app.LoaderManager;
@@ -14,16 +20,23 @@ import android.support.v4.content.Loader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.example.santiago.inventoryapp.data.ProductDbHelper;
 import com.example.santiago.inventoryapp.data.ProductContract.ProductEntry;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class EditActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
     private static final int EXISTING_PRODUCT_LOADER = 0;
@@ -33,6 +46,10 @@ public class EditActivity extends AppCompatActivity implements LoaderManager.Loa
     private Uri currentUri;
     private boolean mProductHasChanged = false;
 
+    private Uri mUri;
+    private static final int PICK_IMAGE_REQUEST = 0;
+    private ImageView mImageView;
+    private Button uploadImage;
     private View.OnTouchListener mTouchListener = new View.OnTouchListener() {
         @Override
         public boolean onTouch(View view, MotionEvent motionEvent) {
@@ -48,9 +65,16 @@ public class EditActivity extends AppCompatActivity implements LoaderManager.Loa
         nameView = (EditText) findViewById(R.id.edit_name);
         priceView = (EditText) findViewById(R.id.price);
         stockView = (EditText) findViewById(R.id.stock);
-
+        uploadImage = (Button) findViewById(R.id.buttonImage);
+        uploadImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buttonImageClick();
+            }
+        });
         Intent intent = getIntent();
         currentUri = intent.getData();
+        mImageView = (ImageView) findViewById(R.id.image_product);
 
         if (currentUri == null) {
             setTitle("Add a product");
@@ -65,8 +89,84 @@ public class EditActivity extends AppCompatActivity implements LoaderManager.Loa
         stockView.setOnTouchListener(mTouchListener);
 
     }
+    private void buttonImageClick() {
+        Intent intent = new Intent();
+
+        if (Build.VERSION.SDK_INT < 19) {
+            intent = new Intent(Intent.ACTION_GET_CONTENT);
+        } else {
+            intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+        }
+        intent.setType("image/*");
+        startActivityForResult(Intent.createChooser(intent, "Select picture"), PICK_IMAGE_REQUEST);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code READ_REQUEST_CODE.
+        // If the request code seen here doesn't match, it's the response to some other intent,
+        // and the below code shouldn't run at all.
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.  Pull that uri using "resultData.getData()"
+
+            if (resultData != null) {
+                mUri = resultData.getData();
+
+                mImageView.setImageBitmap(getBitmapFromUri(mUri));
+            }
+        }
+    }
+    public Bitmap getBitmapFromUri(Uri uri) {
+
+        if (uri == null || uri.toString().isEmpty())
+            return null;
+
+
+        InputStream input = null;
+        try {
+            input = this.getContentResolver().openInputStream(uri);
+
+            // Get the dimensions of the bitmap
+            BitmapFactory.Options bmOptions = new BitmapFactory.Options();
+            bmOptions.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(input, null, bmOptions);
+            input.close();
+
+            // Determine how much to scale down the image
+
+            // Decode the image file into a Bitmap sized to fill the View
+            bmOptions.inJustDecodeBounds = false;
+            bmOptions.inPurgeable = true;
+
+            input = this.getContentResolver().openInputStream(uri);
+            Bitmap bitmap = BitmapFactory.decodeStream(input, null, bmOptions);
+            input.close();
+            return bitmap;
+
+        } catch (FileNotFoundException fne) {
+            return null;
+        } catch (Exception e) {
+            return null;
+        } finally {
+            try {
+                input.close();
+            } catch (IOException ioe) {
+
+            }
+        }
+    }
 
     private void savePet() {
+        Drawable imageImage = mImageView.getDrawable();
+        BitmapDrawable bitmapDrawable = ((BitmapDrawable) imageImage);
+        Bitmap bitmap = bitmapDrawable.getBitmap();
+        //Convert to byte to store
+        ByteArrayOutputStream bos = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 100, bos);
+        byte[] imageByte = bos.toByteArray();
         // Read from input fields
         // Use trim to eliminate leading or trailing white space
         String name = nameView.getText().toString().trim();
@@ -94,6 +194,7 @@ public class EditActivity extends AppCompatActivity implements LoaderManager.Loa
             stock = Integer.parseInt(stockString);
         }
         values.put(ProductEntry.COLUMN_PRODUCT_STOCK, stock);
+        values.put(ProductEntry.COLUMN_IMAGE, imageByte);
 
         // Insert a new row for pet in the database, returning the ID of that new row.
         if (currentUri == null) {
@@ -128,7 +229,10 @@ public class EditActivity extends AppCompatActivity implements LoaderManager.Loa
                 ProductEntry._ID,
                 ProductEntry.COLUMN_PRODUCT_NAME,
                 ProductEntry.COLUMN_PRODUCT_PRICE,
-                ProductEntry.COLUMN_PRODUCT_STOCK};
+                ProductEntry.COLUMN_PRODUCT_STOCK,
+                ProductEntry.COLUMN_IMAGE
+
+        };
 
         // This loader will execute the ContentProvider's query method on a background thread
         return new CursorLoader(this,   // Parent activity context
@@ -148,13 +252,19 @@ public class EditActivity extends AppCompatActivity implements LoaderManager.Loa
             int nameColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_NAME);
             int priceColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_PRICE);
             int stockColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_PRODUCT_STOCK);
+            int imageColumnIndex = cursor.getColumnIndex(ProductEntry.COLUMN_IMAGE);
+
+
             String name = cursor.getString(nameColumnIndex);
             int price = cursor.getInt(priceColumnIndex);
             int stock = cursor.getInt(stockColumnIndex);
+            byte[] image = cursor.getBlob(imageColumnIndex);
 
             nameView.setText(name);
             priceView.setText(Integer.toString(price));
             stockView.setText(Integer.toString(stock));
+            Bitmap lipstickBitmap = BitmapFactory.decodeByteArray(image, 0, image.length);
+            mImageView.setImageBitmap(lipstickBitmap);
         }
     }
 
--- app/src/main/java/com/example/santiago/inventoryapp/data/ProductDbHelper.java
+++ app/src/main/java/com/example/santiago/inventoryapp/data/ProductDbHelper.java
@@ -26,7 +26,7 @@ public class ProductDbHelper extends SQLiteOpenHelper {
                 + ProductEntry.COLUMN_PRODUCT_NAME + " TEXT NOT NULL, "
                 + ProductEntry.COLUMN_PRODUCT_PRICE + " INTEGER, "
                 + ProductEntry.COLUMN_PRODUCT_STOCK + " INTEGER NOT NULL DEFAULT 0,"
-                + ProductEntry.COLUMN_IMAGE +" TEXT " + ")";
+                + ProductEntry.COLUMN_IMAGE +" BLOB " + ")";
 
         // Execute the SQL statement
         db.execSQL(SQL_CREATE_PRODUCTS_TABLE);
